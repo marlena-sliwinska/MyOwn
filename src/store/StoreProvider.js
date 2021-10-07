a85795b249a7578b987fac0423c35cd0f24e08db
Marlena Śliwińska <marlena.sliwinska@gmail.coml.com>
2021-10-07 13:14:44 +0200

    initial store removed

--- src/store/StoreProvider.js
+++ src/store/StoreProvider.js
@@ -8,39 +8,7 @@ function StoreProvider({ children }) {
     const [user, setUser] = useState({
         name: '',
     })
-    const [lists, dispatch] = useReducer(listsReducers, [
-        {
-            title: 'Zostać frontend developerem',
-            id: 666,
-            tasks: [
-                { id: 45, content: "do końca października opanuje hooki", isDone: false },
-                { id: 456, content: "w listopadzie pierwsze dwa tygodnie poświece na gita", isDone: true }
-            ],
-            created: getCurrentDate(),
-            updated: getCurrentDate(),
-        },
-        {
-            title: 'Zrobić hanumasane',
-            id: 7878,
-            tasks: [
-                { id: 55, content: "opświecać 30 minut dziennie na rozciąganie", isDone: false },
-                { id: 12, content: "poczytać o pozycji w Anatomi jogi", isDone: true },
-            ],
-            created: getCurrentDate(),
-            updated: getCurrentDate(),
-        },
-        {
-            title: 'Zrobić obiad',
-            id: 6657657612,
-            tasks: [
-                { id: 456455, content: "kupic buraki", isDone: false },
-                { id: 6969, content: "upiec jabłka", isDone: false }
-            ],
-            created: getCurrentDate(),
-            updated: getCurrentDate(),
-        },
-
-    ])
+    const [lists, dispatch] = useReducer(listsReducers, [])
     const [openedList, setOpenedList] = useState(null)
     const [createNewList, setCreateNewList] = useState(false)
 
